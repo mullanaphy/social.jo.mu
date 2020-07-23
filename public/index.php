06cdd577f8b521cdbb0e978fa4e4b2fab8cf059a
mullanaphy <mullanaphy@gmail.com>
2020-07-23 01:40:08 +0000

    Added a link to the source code and icons.

--- public/index.php
+++ public/index.php
@@ -11,7 +11,11 @@
 
   $hostName = str_replace('.jo.mu', '', $_SERVER['HTTP_HOST']);
   if (isset($socialMedia[$hostName])) {
-    header('Location: ' . $socialMedia[$hostName]);
+    $requestUri = str_replace('/', '', $_SERVER['REQUEST_URI']);
+    if ($requestUri) {
+      $requestUri = '/' . $requestUri;
+    }
+    header('Location: ' . $socialMedia[$hostName] . $requestUri);
     exit;
   }
   
@@ -24,22 +28,30 @@
       '<title>Around The Web</title>',
       '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous"/>',
       '<script src="https://kit.fontawesome.com/686cbf714c.js" crossorigin="anonymous"></script>',
-      '<style type="text/css">li { text-transform: capitalize; }</style>',
+      '<style type="text/css">#social-media-links li { text-transform: capitalize; }</style>',
     '</head>',
     '<body>',
       '<div class="container">',
         '<h1>Around The Web</h1>',
+        '<hr/>',
       '</div>',
-      '<hr/>',
       '<div class="container">',
-        '<ul class="list-unstyled">',
+        '<ul class="list-unstyled" id="social-media-links">',
           implode('', array_map(function($key, $value) {
-            return '<li><a href="' . $value . '" rel="nofollow"><i class="fas fa-external-link-alt"></i> ' . $key . '</a></li>';
+            $pack = 'chess' === $key
+              ? 'fas'
+              : 'fab';
+            return '<li><a href="' . $value . '" rel="nofollow"><i class="' . $pack . ' fa-' . $key . '"></i> ' . $key . '</a></li>';
           }, array_keys($socialMedia), array_values($socialMedia))),
         '</ul>',
+        '<hr/>',
       '</div>',
-      '<hr/>',
-      '<div class="container"><strong><a href="https://john.mu">john.mu</a></strong></div>',
+      '<div class="container">',
+        '<ul class="list-unstyled">',
+          '<li><a href="https://john.mu"><i class="fas fa-home"></i> john.mu</a></li>',
+          '<li><a href="https://github.jo.mu/social.jo.mu" rel="nofollow"><i class="fab fa-github-alt"></i> Source Code</a></li>',
+        '</ul>',
+      '</div>',
     '</body>',
   '</html>';
 
